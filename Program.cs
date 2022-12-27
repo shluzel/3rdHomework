90808065890b4effbf39f0bb02858fd1c6bc5e74
Darya <dasha.890@yandex.ru>
2022-12-27 23:04:52 +0500

    решили первую задачу

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write ("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int firsthalf = a/10000 *10 + a/1000 %10;
+int secondhalf = a%10*10 +a%100/10;
+if (firsthalf == secondhalf) {
+    Console.Write($"Число {a} является палиндромом." );
+}
+else {
+    Console.Write($"Число {a} не является палиндромом." );
+}
