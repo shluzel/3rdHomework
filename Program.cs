43a12b5b8ce798bc9fa6f62f84157dbae6d1c3df
Darya <dasha.890@yandex.ru>
2022-12-27 23:21:04 +0500

    решили третью задачу

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ else {
 */
 
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
 Console.Write ("Введите последовательно (x,y,z) координаты первой точки в 3D пространстве, нажимая Enter после каждого значения: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 int y1 = Convert.ToInt32(Console.ReadLine());
@@ -29,3 +30,12 @@ int z2 = Convert.ToInt32(Console.ReadLine());
 double l = Math.Sqrt (Math.Pow((x2-x1),2)+ Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
 
 Console.Write($"Расстояние между двумя точками ({x1},{y1},{z1}) и ({x2},{y2},{z2}) получилось равным {l:f5}." );
+*/
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write ("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Кубы чисел от 1 до введенного числа: ");
+for (int i = 1; i<=a; i++) {
+    Console.Write ($"{Math.Pow(i,3)} ");
+}
