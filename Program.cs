e2c6567b34d02e3dee743f502f3f6711c89ed0c1
Darya <dasha.890@yandex.ru>
2022-12-27 23:16:17 +0500

    решили вторую задачу

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 Console.Clear();
 
 //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
 Console.Write ("Введите пятизначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int firsthalf = a/10000 *10 + a/1000 %10;
@@ -12,3 +13,19 @@ if (firsthalf == secondhalf) {
 else {
     Console.Write($"Число {a} не является палиндромом." );
 }
+*/
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Write ("Введите последовательно (x,y,z) координаты первой точки в 3D пространстве, нажимая Enter после каждого значения: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите последовательно (x,y,z) координаты второй точки в 3D пространстве, нажимая Enter после каждого значения: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double l = Math.Sqrt (Math.Pow((x2-x1),2)+ Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+
+Console.Write($"Расстояние между двумя точками ({x1},{y1},{z1}) и ({x2},{y2},{z2}) получилось равным {l:f5}." );
